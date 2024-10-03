e519add3970ddeaaff9fcbea4e31d088e4f0ba0e
Jason Balan <minecraftjason747@gmail.com>
2024-10-03 22:09:12 +0530

    Program2.java

--- Program2.java
+++ Program2.java
@@ -4,6 +4,7 @@ public class Duplicate
   public static void main(String[] args)
    {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size of original array: ");
     int n = sc.nextInt(), arr[] = new int[n], arr1[] = new int[n], j = 0;
     boolean check = true;
      
@@ -12,7 +13,7 @@ public class Duplicate
       arr[i] = sc.nextInt();
      }
     
-    System.out.print("Original: ");  
+    System.out.print("\nOriginal: ");  
     for (int i = 0; i < n; i++)
      {
       System.out.print(arr[i] + ", ");
@@ -20,14 +21,23 @@ public class Duplicate
      
     for (int i = 0; i < n - 1; i++) 
      {
-	  if (arr[i] != arr[i + 1])
+	  if(arr[i] != arr[i + 1])
 	    arr1[j++] = arr[i];
 	 }
-	  
-	 for (int i = 0; i < j; i++)
-	  {
-	   arr[i] = arr1[i];
-	  }
+	 
+	for (int i = 0; i < j; i++) 
+	 {
+      if (arr1[i] == arr[n - 1]) 
+       {
+        check = false;
+        break;
+       }
+     }
+
+    if (check == true) 
+     {
+      arr1[j++] = arr[n - 1];
+     }
 	 
 	 System.out.print("\nAfter Duplicates Removed: ");  
      for (int i = 0; i < n; i++)
